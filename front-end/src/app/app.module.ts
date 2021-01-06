1724ba9301dd7d14e1a9783fa04df71c6c6700a7
Marcus Veloso <desenvolvedoroficial@gmail.com>
2021-01-06 10:50:21 -0300

    add navegacao module

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NavegacaoModule } from '../app/navegacao/navegacao.module'
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NavegacaoModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
